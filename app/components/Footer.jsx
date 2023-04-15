19edc8db1be54d3e72f5d1c0d620a0b20d44c2d5
Shambhav shandilya <shambhavse15@gmail.com>
2023-04-15 21:03:24 +0530

    about page done

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -40,7 +40,7 @@ export default function Footer() {
                                         </a>
                                     </li>
                                     <li>
-                                        <a href="mailto: shambhavse15@gmail.com" target='blank'>
+                                        <a href="mailto:shambhavse15@gmail.com" target='blank'>
                                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path d="M3 3H21C21.5523 3 22 3.44772 22 4V20C22 20.5523 21.5523 21 21 21H3C2.44772 21 2 20.5523 2 20V4C2 3.44772 2.44772 3 3 3ZM20 7.23792L12.0718 14.338L4 7.21594V19H20V7.23792ZM4.51146 5L12.0619 11.662L19.501 5H4.51146Z" fill="#000"></path></svg>
                                         </a>
                                     </li>
@@ -61,7 +61,7 @@ export default function Footer() {
                                         </a>
                                     </li>
                                     <li>
-                                        <a href="mailto: wacky.piyush@gmail.com" target='blank'>
+                                        <a href="mailto:wacky.piyush@gmail.com" target='blank'>
                                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path d="M3 3H21C21.5523 3 22 3.44772 22 4V20C22 20.5523 21.5523 21 21 21H3C2.44772 21 2 20.5523 2 20V4C2 3.44772 2.44772 3 3 3ZM20 7.23792L12.0718 14.338L4 7.21594V19H20V7.23792ZM4.51146 5L12.0619 11.662L19.501 5H4.51146Z" fill="#000"></path></svg>
                                         </a>
                                     </li>
@@ -82,7 +82,7 @@ export default function Footer() {
                                         </a>
                                     </li>
                                     <li>
-                                        <a href="mailto: shambhavse15@gmail.com" target='blank'>
+                                        <a href="mailto:shambhavse15@gmail.com" target='blank'>
                                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path d="M3 3H21C21.5523 3 22 3.44772 22 4V20C22 20.5523 21.5523 21 21 21H3C2.44772 21 2 20.5523 2 20V4C2 3.44772 2.44772 3 3 3ZM20 7.23792L12.0718 14.338L4 7.21594V19H20V7.23792ZM4.51146 5L12.0619 11.662L19.501 5H4.51146Z" fill="#000"></path></svg>
                                         </a>
                                     </li>
@@ -103,7 +103,7 @@ export default function Footer() {
                                         </a>
                                     </li>
                                     <li>
-                                        <a href="mailto: shambhavse15@gmail.com">
+                                        <a href="mailto:shambhavse15@gmail.com">
                                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path d="M3 3H21C21.5523 3 22 3.44772 22 4V20C22 20.5523 21.5523 21 21 21H3C2.44772 21 2 20.5523 2 20V4C2 3.44772 2.44772 3 3 3ZM20 7.23792L12.0718 14.338L4 7.21594V19H20V7.23792ZM4.51146 5L12.0619 11.662L19.501 5H4.51146Z" fill="#000"></path></svg>
                                         </a>
                                     </li>
