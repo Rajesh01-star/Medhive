1e50a89f5d8312f47340b4d8792ec4afc4da3bbe
Rajesh01-star <rajeshbehera489@gmail.com>
2024-11-01 18:54:33 +0530

    some fixes

--- app/support/url.js
+++ app/support/url.js
@@ -3,6 +3,7 @@ const carouselFetchUrl = "https://medhive-backend.vercel.app/hospitals";
 // const newsApiKey = "8f3532ad8cbb4a5ba928b437ce5d303d";
 // const newsApiUrl = `https://newsapi.org/v2/top-headlines?country=in&source=bbc=news&category=health&language=en&sortBy=top&apiKey=${newsApiKey}`;
 const newsApiUrl = 'https://medhive-backend.vercel.app/News';
+// const newsApiUrl = 'https://http://localhost:4000/News';
 const fallBackImageUrl =
   "https://images.unsplash.com/photo-1551076805-e1869033e561?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1332&q=80";
 const searchHospitalUrl = 'https://medhive-backend.vercel.app/Hospitaldetails?'
